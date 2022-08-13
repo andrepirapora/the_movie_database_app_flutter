a22e621f5b0fe36927a920b36dbca503d35ce0a0
Andre Teixeira <andreteixeira628@gmail.com>
2022-08-13 09:02:36 -0300

    test: ensure HttpAdapter calls post with correct values

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -5,7 +5,31 @@ import 'package:test/test.dart';
 import 'package:the_movie_db/data/http/http.dart';
 import 'package:the_movie_db/infra/http/http.dart';
 
-class ClientSpy extends Mock implements Client {}
+class ClientSpy extends Mock implements Client {
+  ClientSpy() {
+    mockPost(200);
+    mockPut(200);
+    mockGet(200);
+  }
+
+  When mockPostCall() => when(() => this
+      .post(any(), body: any(named: 'body'), headers: any(named: 'headers')));
+  void mockPost(int statusCode, {String body = '{"any_key":"any_value"}'}) =>
+      mockPostCall().thenAnswer((_) async => Response(body, statusCode));
+  void mockPostError() => when(() => mockPostCall().thenThrow(Exception()));
+
+  When mockPutCall() => when(() => this
+      .put(any(), body: any(named: 'body'), headers: any(named: 'headers')));
+  void mockPut(int statusCode, {String body = '{"any_key":"any_value"}'}) =>
+      mockPutCall().thenAnswer((_) async => Response(body, statusCode));
+  void mockPutError() => when(() => mockPutCall().thenThrow(Exception()));
+
+  When mockGetCall() =>
+      when(() => this.get(any(), headers: any(named: 'headers')));
+  void mockGet(int statusCode, {String body = '{"any_key":"any_value"}'}) =>
+      mockGetCall().thenAnswer((_) async => Response(body, statusCode));
+  void mockGetError() => when(() => mockGetCall().thenThrow(Exception()));
+}
 
 void main() {
   late HttpAdapter sut;
@@ -13,14 +37,41 @@ void main() {
   late String url;
 
   setUp(() {
-    url = faker.internet.httpsUrl();
     client = ClientSpy();
     sut = HttpAdapter(client: client);
+  });
+
+  setUpAll(() {
+    url = faker.internet.httpUrl();
+    registerFallbackValue(Uri.parse(url));
   });
 
   test("Should throw ServerError if invalid method is provided", () async {
     final future = sut.request(url: url, method: 'invalid_method');
 
     expect(future, throwsA(HttpError.serverError));
+  });
+
+  test("Should call post with correct values", () async {
+    await sut.request(url: url, method: 'post', body: {'any_key': 'any_value'});
+    verify(() => client.post(Uri.parse(url),
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json'
+        },
+        body: '{"any_key":"any_value"}'));
+
+    await sut.request(
+        url: url,
+        method: 'post',
+        body: {'any_key': 'any_value'},
+        headers: {'any_header': 'any_value'});
+    verify(() => client.post(Uri.parse(url),
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json',
+          'any_header': 'any_value'
+        },
+        body: '{"any_key":"any_value"}'));
   });
 }
